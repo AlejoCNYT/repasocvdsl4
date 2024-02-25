eb2d36ac23270d3ff0406ad7935845541b22246e
AlejoCNYT <daniel.acero@mail.escuelaing.edu.co>
2024-02-25 15:39:17 -0500

    especificación métodos

---
+++ src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hangman.setup.guice;
+
+/**
+ * @author 2106913
+ */
+
+public class HangmanFactoryServices extends com.google.inject.AbstractModule {
+
+    @Override
+    protected void configure() {
+        /* Guice dependency injection */
+        // bind(Interface.class).to(Concrete.class);
+    }
+
+}
